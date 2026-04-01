b4d2ea5d67748c2e79f9c528fa6d83637fcc94fb
fluoronator <66132286+fluoronator@users.noreply.github.com>
2026-04-01 09:13:19 -0500

    Initialize map with OpenStreetMap tiles

--- js/map.js
+++ js/map.js
@@ -1,1 +1,11 @@
 
+// Create map
+window.map = L.map('map', {
+    zoomControl: false,
+    attributionControl: false
+}).setView([0,0], 15);
+
+// Tiles
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
